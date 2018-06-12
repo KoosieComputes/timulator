f3a4b2763fcc37b08b883f27ae8c604bbb3b3acb
Jannie Theunissen <jannie@onesheep.org>
2018-06-12 20:48:06 +0200

    karretjie ry

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var ref;
+
 var kar = {
-	x: 200, 
-	y: 20,
-	dx: 0, 
+	x: 700, 
+	y: 200,
+	dx: -10,
 	dy: 0, 
 
-	draw: function(ctx) {
-		ctx.fillRect(this.x, this.y,40,15);
+	step: function() {
+		if (this.x < 0 || this.x > canvas.width) {
+			this.dx = -1 * this.dx;
+		}
+		this.x += this.dx;
+  		this.y += this.dy;
+	},
+
+	draw: function() {
+		ctx.fillRect(this.x, this.y, 40, 15);
 	}
 }
 
-var ctx = document.getElementById('canvas').getContext('2d');
-// ctx.strokeStyle = 'rgb(255, 0, 0)';
-// ctx.fillStyle = 'rgb(255, 0, 0)';
-kar.draw(ctx);
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  kar.step();
+  kar.draw();
+  ref = window.requestAnimationFrame(draw);
+}
+
+canvas.addEventListener('mouseover', function(e) {
+  ref = window.requestAnimationFrame(draw);
+});
+
+canvas.addEventListener('mouseout', function(e) {
+  window.cancelAnimationFrame(ref);
+});
+
+kar.draw();
